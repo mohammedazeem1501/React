6f6ef2077a8c8592de71636a2deb824771e5948d
mohammedazeem1501 <mohammed.azeem@tothenew.com>
2020-05-07 12:16:45 +0530

    Added server connection to the same app

---
+++ burger-builder/build/precache-manifest.bc8dc1d6f68b7a7408c5446434cab9a3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d7da4610633ae4a04f16e74d0b51d12c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ddc066f8e023de35b008",
+    "url": "/static/css/main.d8b266b0.chunk.css"
+  },
+  {
+    "revision": "b02c549eabd9fb00c79b",
+    "url": "/static/js/2.920da065.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.920da065.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ddc066f8e023de35b008",
+    "url": "/static/js/main.f2f2b142.chunk.js"
+  },
+  {
+    "revision": "416cbe68a70f736cc07a",
+    "url": "/static/js/runtime-main.3a0a1a0f.js"
+  },
+  {
+    "revision": "b8503d262bffbfb7c67fd6762963e7d1",
+    "url": "/static/media/burger-logo.b8503d26.png"
+  }
+]);

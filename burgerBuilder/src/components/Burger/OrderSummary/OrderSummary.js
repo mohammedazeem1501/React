6fdf37cf2db1cceab673ac284f18a8855940d185
mohammedazeem1501 <mohammed.azeem@tothenew.com>
2020-05-10 20:04:44 +0530

    Route and FormValidation Added in Burger Builder App

---
+++ burgerBuilder/src/components/Burger/OrderSummary/OrderSummary.js
@@ -0,0 +1,30 @@
+import React,{Component} from 'react'
+import Aux from '../../../hoc/Auxiliary/Auxiliary'
+import Button from '../../UI/Button/Button'
+class OrderSummary extends Component{ 
+    componentWillUpdate(){
+        console.log("[ordersummary] will update"); 
+    }
+    render(){
+        const ingredientSummary = Object.keys(this.props.ingredients)
+        .map(igKey => {
+            return (<li key={igKey}>
+                <span style={{ textTransform: "capitalize" }}>{igKey}</span> : {this.props.ingredients[igKey]}
+            </li>)
+        })
+        return(
+            <Aux>
+            <h3>Your Order Summary  </h3>
+            <p>You have chosen a <strong>Delicious burger </strong>with the following ingredients : </p>
+            <ul>
+                {ingredientSummary}
+            </ul>
+            <p><strong>Payable Price - Rs.{this.props.price}</strong></p>
+            <p>Continue to Checkout ..?</p>
+            <Button btnType="Danger" clicked={this.props.purchaseCanceled}>CANCEL</Button>
+            <Button btnType="Success" clicked={this.props.purchaseContinued}>CONTINUE</Button>
+        </Aux>
+        )
+    }
+}
+export default OrderSummary

7472a17c5032fed3b3cfc41a8e72d81c69b31e68
mohammedazeem1501 <mohammed.azeem@tothenew.com>
2020-05-13 11:13:27 +0530

    implemented Advance Redux

--- src/App.js
+++ src/App.js
@@ -1,26 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import Layout from './hoc/Layout/Layout';
+import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
+import Checkout from './containers/Checkout/Checkout';
+import { Route, Switch } from 'react-router-dom'
+import Orders from './containers/Orders/Orders'
+class App extends Component {
+  // state = {
+  //   show : true
+  // }
+  // componentDidMount(){ 
+  //   setTimeout(()=>{
+  //     this.setState({show:false})
+  //   },5000)
+  // }
+  render() {
+    return (
+      <div >
+        <Layout>
+          <Switch>
+            <Route path="/checkout" component={Checkout} />
+            <Route path="/orders" exact component={Orders} />
+            <Route path="/" exact component={BurgerBuilder} />
+          </Switch>
+        </Layout>
+      </div>
+    );
+  }
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
 }
 
 export default App;
